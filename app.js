4f882d7776a851873e6c2f37b54cffdf704ddd22
mdmuche <abulamartins@gmail.com>
2024-09-09 19:19:52 +0100

    update app allowed methods

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
       return callback(null, true);
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
